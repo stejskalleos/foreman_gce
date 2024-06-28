d28ae12a6a0f143d9157251d0c4e57a4897609ff
akumari <akumari@redhat.com>
2024-06-28 15:53:59 +0530

    Fix Style/TernaryParentheses cop

--- test/test_google_helper.rb
+++ test/test_google_helper.rb
@@ -16,7 +16,7 @@ class GoogleTestCase < ActiveSupport::TestCase
   let(:google_access_token) { 'ya29.c.stubbed_token' }
   let(:gce_cr) { FactoryBot.create(:compute_resource, :google_gce) }
   let(:google_project_id) { gce_cr.google_project_id }
-  let(:gauth_json) { (ENV['VCR'] == '1' ? ENV['GCE_AUTH'] : nil) || gce_cr.password }
+  let(:gauth_json) { ((ENV['VCR'] == '1') ? ENV['GCE_AUTH'] : nil) || gce_cr.password }
 
   setup do
     ::Signet::OAuth2::Client.any_instance.stubs(fetch_access_token!: google_access_token) unless ENV['VCR'] == '1'
